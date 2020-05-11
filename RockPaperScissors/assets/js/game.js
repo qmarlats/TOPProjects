246ec1196525cb94aec21c593683528234cfbb0f
Quentin Marlats <qmarlats+github@outlook.com>
2020-05-11 23:13:59 +0200

    Add Rock, Paper, Scissors UI

--- RockPaperScissors/assets/js/game.js
+++ RockPaperScissors/assets/js/game.js
@@ -1,96 +1,233 @@
-const ROCK = 0;  // Rock index
-const PAPER = 1;  // Paper index
-const SCISSORS = 2;  // Scissors index
+class Player {
+    constructor(id, name, type="HUMAN") {
+        // Initialize defaults
+        this.DEFAULT_CHOICE = "pending";
+        this.DEFAULT_SCORE = 0;
 
-// Fights winner
-const WINNING_MATRIX = [
-    [null, PAPER, ROCK],
-    [PAPER, null, SCISSORS],
-    [ROCK, SCISSORS, null]
-];
+        // Initialize outputs
+        this.nameOutput = document.querySelector(`#player-${id}-name`);
+        this.choiceOutput = document.querySelector(`#player-${id}-choice`);
+        this.scoreOutput = document.querySelector(`#player-${id}-score`);
 
-// Choice to string table (0 = Rock, 1 = Paper, 2 = Scissors)
-const CHOICE_TO_STRING = ["Rock", "Paper", "Scissors"]
-
-function userPlay(message="Rock, Paper or Scissors?") {
-    // Ask user for his choice
-    let userChoice = prompt(message);
-
-    // Clean and return user input
-    userChoice = userChoice.toLowerCase();
-    if (userChoice === "rock") {
-        return ROCK;
+        // Initialize user information
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.choice = this.DEFAULT_CHOICE;
+        this.score = this.DEFAULT_SCORE;
     }
-    else if (userChoice === "paper") {
-        return PAPER;
+
+    get name() {
+        return this._name;
     }
-    else if (userChoice === "scissors") {
-        return SCISSORS;
+
+    set name(name) {
+        this._name = name;
+
+        // Refresh output
+        this.nameOutput.textContent = this.name;
     }
-    else {
-        return userPlay("Please enter a valid choice. Rock, Paper or Scissors?");
+
+    get choice() {
+        return this._choice;
+    }
+
+    set choice(choice) {
+        const choices = ["rock", "paper", "scissors", "pending", "error"];
+
+        this._choice = choices.includes(choice) ? choice : "error";
+
+        // Refresh output
+        this.choiceOutput.classList.remove(...choices);
+        this.choiceOutput.classList.add(this.choice);
+    }
+
+    get score() {
+        return this._score;
+    }
+
+    set score(score) {
+        this._score = score;
+
+        // Refresh output
+        this.scoreOutput.textContent = this.score;
+    }
+
+    reset() {
+        // Reset to defaults
+        this.choice = this.DEFAULT_CHOICE;
+        this.score = this.DEFAULT_SCORE;
+    }
+
+    setRandomChoice() {
+        // Possible choices
+        const choices = ["rock", "paper", "scissors"];
+
+        // Set random choice
+        this.choice = choices[Math.floor(Math.random() * 3)];
+    }
+
+    incrementScore(increment=1) {
+        this.score += increment;
     }
 }
 
-function computerPlay() {
-    // Return a random number between 0 and 2 (included)
-    return Math.floor(Math.random() * 3);
+class Round {
+    constructor(...players) {
+        // Initialize players
+        this.players = players;
+
+        // Initialize winner
+        this.winner = null;
+    }
+
+    play() {
+        // Map choices to indexes
+        const choices = {rock: 0, paper: 1, scissors: 2};
+
+        // Compute winner
+        this.winner = this.players.reduce((a, b) => {
+            const aChoice = choices[a.choice];
+            const bChoice = choices[b.choice];
+            const winnerMatrix = [
+                [null, b, a],
+                [a, null, b],
+                [b, a, null]
+            ];
+
+            return winnerMatrix[aChoice][bChoice];
+        });
+
+        // Increment winner score
+        if (this.winner) {
+            this.winner.incrementScore();
+        }  // else it's a draw
+    }
 }
 
-function play(userChoice, computerChoice) {
-    // Return winner
-    if (WINNING_MATRIX[userChoice][computerChoice] === userChoice) {
-        return "USER";
+class Game {
+    constructor(...players) {
+        // Initialize output
+        this.winnerOutput = document.querySelector(`#winner-name`);
+
+        // Initialize players
+        this.players = players;
+        this.players.forEach(player => player.reset());
+
+        // Initialize game
+        this.maximumScore = MAXIMUM_SCORE;
+        this.round = new Round(...players);
+        this.winner = null;
     }
-    else if (WINNING_MATRIX[userChoice][computerChoice] === computerChoice) {
-        return "COMPUTER";
+
+    get firstPlayer() {
+        return this.players[0];
     }
-    else {
-        return "NOBODY";
+
+    get currentPlayer() {
+        return this._currentPlayer || this.firstPlayer;
     }
-}
 
-function game() {
-    // Welcome message
-    console.log("Let's play Rock, Paper, Scissors in 5 rounds.");
+    set currentPlayer(player) {
+        this._currentPlayer = player;
+    }
 
-    // Play 5 times
-    let userWins = 0, computerWins = 0;
-    for (i = 0; i < 5; i++) {
-        // Prompt user and computer for their choices
-        userChoice = userPlay();
-        computerChoice = computerPlay();
+    get nextPlayer() {
+        const currentPlayerIndex = this.players.indexOf(this.currentPlayer);
+        const lastPlayerIndex = this.players.indexOf(this.lastPlayer);
 
-        // Log choices
-        console.log(`User choice: ${CHOICE_TO_STRING[userChoice]}`);
-        console.log(`Computer choice: ${CHOICE_TO_STRING[computerChoice]}`);
+        return (currentPlayerIndex < lastPlayerIndex)
+            ? this.players[currentPlayerIndex + 1]
+            : this.firstPlayer;
+    }
 
-        // Play round
-        winner = play(userChoice, computerChoice);
+    get lastPlayer() {
+        return this.players[this.players.length - 1];
+    }
 
-        // Save scores and log round winner
-        if (winner === "USER") {
-            userWins++;
-            console.log("User wins!");
-        }
-        else if (winner === "COMPUTER") {
-            computerWins++;
-            console.log("Computer wins!");
+    get maximumScoreReached() {
+        return this.players.some(player => {
+            return player.score === this.maximumScore;
+        });
+    }
+
+    get winner() {
+        return this._winner;
+    }
+
+    set winner(winner) {
+        this._winner = winner;
+
+        // Refresh output
+        if (winner) {
+            this.winnerOutput.textContent = `${this.winner.name} wins! `;
+
+            const restartGame = document.createElement('span');
+            restartGame.setAttribute('id', 'restart-game');
+            restartGame.textContent = "Restart game?";
+            this.winnerOutput.appendChild(restartGame);
         }
         else {
-            console.log("Nobody wins...");
+            this.winnerOutput.innerHTML = "";
         }
     }
 
-    // Log global winner
-    let score = `User ${userWins} - ${computerWins} Computer`;
-    if (userWins > computerWins) {
-        console.log(`User wins the game! (${score})`);
+    switchToNextPlayer() {
+        this.currentPlayer = this.nextPlayer;
     }
-    else if (computerWins > userWins) {
-        console.log(`Computer wins the game! (${score})`);
-    }
-    else {
-        console.log(`Nobody wins the game... (${score})`);
+
+    playRound(choice) {
+        if (!this.maximumScoreReached) {
+            switch (this.currentPlayer.type) {
+                case "HUMAN":
+                    this.currentPlayer.choice = choice;
+                    break;
+                case "COMPUTER":
+                    this.currentPlayer.setRandomChoice();
+                    break;
+            }
+
+            if (this.currentPlayer === this.lastPlayer) {
+                this.round.play();
+            }
+
+            if (this.maximumScoreReached) {
+                this.winner = this.round.winner;
+            }
+            else {
+                this.switchToNextPlayer();
+
+                if (this.currentPlayer.type === "COMPUTER") {
+                    this.playRound();
+                }
+            }
+        }
     }
 }
+
+// Default settings
+const MAXIMUM_SCORE = 5;
+
+// Players
+let players = [
+    new Player(1, "Human"),
+    new Player(2, "Computer", "COMPUTER")
+];
+
+// Start new game
+let game = new Game(...players);
+
+// Play round
+const actions = document.querySelectorAll('#actions button[name=action]');
+actions.forEach(action => {
+    action.addEventListener('click', () => {
+        game.playRound(action.value);
+    });
+});
+
+// Restart the game
+document.addEventListener('click', element => {
+    if (element.target.id === 'restart-game'){
+        game = new Game(...players);
+    }
+});
